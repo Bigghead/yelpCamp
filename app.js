5a2aad4cdcd9f94a479017c9330e46de47635ca6
Shav <shavenchew@yahoo.com>
2016-10-28 16:41:08 -0700

    'Added EJS Partials Templates'

--- app.js
+++ app.js
@@ -7,6 +7,21 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));  //css files
 
+//Home Page
+app.get('/', function(req, res){
+  res.render('landing');
+});
+
+//Show All Campgrounds
+app.get('/campgrounds', function(req, res){
+  //campgrounds will be in a database. This will get passed to a view
+  var campgrounds = [
+    {name: 'Salmon Creek', image: 'https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg'},
+    {name: 'Granite Hill', image: 'http://www.photosforclass.com/download/4369518024'},
+    {name: 'Salmon Creek', image: 'http://www.photosforclass.com/download/4684194306'}
+  ];
+  res.render('campgrounds', {camps : campgrounds});
+});
 
 app.listen(3000, function(){
   console.log('Camp Server Started');
