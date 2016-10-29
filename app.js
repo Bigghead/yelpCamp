0fe5e51e06b09efc5085d0772045dccace61dd32
Shav <shavenchew@yahoo.com>
2016-10-28 17:19:18 -0700

    'Working Post Route With Hardcoded data'

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));  //css files
 
+var campgrounds = [
+  {name: 'Salmon Creek', image: 'https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg'},
+  {name: 'Granite Hill', image: 'http://www.photosforclass.com/download/4369518024'},
+  {name: 'Salmon Creek', image: 'http://www.photosforclass.com/download/4684194306'}
+];
+
 //Home Page
 app.get('/', function(req, res){
   res.render('landing');
@@ -15,12 +21,24 @@ app.get('/', function(req, res){
 //Show All Campgrounds
 app.get('/campgrounds', function(req, res){
   //campgrounds will be in a database. This will get passed to a view
-  var campgrounds = [
-    {name: 'Salmon Creek', image: 'https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg'},
-    {name: 'Granite Hill', image: 'http://www.photosforclass.com/download/4369518024'},
-    {name: 'Salmon Creek', image: 'http://www.photosforclass.com/download/4684194306'}
-  ];
   res.render('campgrounds', {camps : campgrounds});
+});
+
+//add new campground
+app.post('/campgrounds', function(req, res){
+  //get data from form and add to camps database
+  var campName = req.body.campName;
+  var campImage = req.body.campImage;
+  var newCampGround = {name: campName, image: campImage};
+  campgrounds.push(newCampGround);
+  //redirect back to /campgrounds
+  // res.render('campgrounds');
+  res.redirect('campgrounds');
+});
+
+//page that has a form to send data to the post route above
+app.get('/campgrounds/new', function(req, res){
+  res.render('new');
 });
 
 app.listen(3000, function(){
